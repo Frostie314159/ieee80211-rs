e297e617cbeb99d273fe751727c166d19eeb98a4
Frostie314159 <iam.an.programmer@gmail.com>
2023-12-20 11:55:11 +0100

    Adjusted derives for FragSeqInfo.

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -1,7 +1,7 @@
 use macro_bits::{bit, bitfield};
 
 bitfield! {
-    #[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+    #[derive(Clone, Copy, Debug, Default, Hash, PartialEq, Eq)]
     pub struct FragSeqInfo: u16 {
         pub fragment_number: u8 => bit!(0,1,2,3),
         pub sequence_number: u16 => bit!(4,5,6,7,8,9,10,11,12,13,14,15)
