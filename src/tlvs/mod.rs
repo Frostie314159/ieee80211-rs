9fb3426f6a0a1c7abd7523fd9a478f069a4dc4bf
Frostie314159 <iam.an.programmer@gmail.com>
2023-12-21 16:51:48 +0100

    Reworked TLVs.

--- src/tlvs/mod.rs
+++ src/tlvs/mod.rs
@@ -1,9 +1,11 @@
+use core::marker::PhantomData;
+
 use macro_bits::serializable_enum;
 use scroll::{
     ctx::{MeasureWith, TryFromCtx, TryIntoCtx},
     Endian, Pwrite,
 };
-use tlv_rs::TLV;
+use tlv_rs::{TLV, raw_tlv::RawTLV};
 
 use self::{ssid::SSIDTLV, supported_rates::SupportedRatesTLV};
 
@@ -19,32 +21,33 @@ serializable_enum! {
     }
 }
 
-pub type RawIEEE80211TLV<'a, Payload> = TLV<'a, u8, TLVType, u8, Payload>;
+pub type RawIEEE80211TLV<'a> = RawTLV<'a, u8, u8>;
+type TypedIEEE80211TLV<Payload> = TLV<u8, u8, TLVType, Payload>;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum IEEE80211TLV<'a> {
     SSID(SSIDTLV<'a>),
     SupportedRates(SupportedRatesTLV),
-    Unknown(RawIEEE80211TLV<'a, &'a [u8]>),
+    Unknown(RawIEEE80211TLV<'a>),
 }
 impl MeasureWith<()> for IEEE80211TLV<'_> {
     fn measure_with(&self, ctx: &()) -> usize {
         2 + match self {
             Self::SSID(tlv) => tlv.measure_with(ctx),
             Self::SupportedRates(tlv) => tlv.measure_with(ctx),
-            Self::Unknown(tlv) => tlv.data.len(),
+            Self::Unknown(tlv) => tlv.slice.len(),
         }
     }
 }
 impl<'a> TryFromCtx<'a> for IEEE80211TLV<'a> {
     type Error = scroll::Error;
     fn try_from_ctx(from: &'a [u8], _ctx: ()) -> Result<(Self, usize), Self::Error> {
-        let (tlv, len) = <RawIEEE80211TLV<'a, &'a [u8]> as TryFromCtx<'a, Endian>>::try_from_ctx(
+        let (tlv, len) = <RawIEEE80211TLV<'a> as TryFromCtx<'a, Endian>>::try_from_ctx(
             from,
             Endian::Little,
         )?;
         Ok((
-            match tlv.tlv_type {
+            match TLVType::from_representation(tlv.tlv_type) {
                 TLVType::SSID => Self::SSID(SSIDTLV::try_from_ctx(from, ()).map(|(tlv, _)| tlv)?),
                 TLVType::SupportedRates => Self::SupportedRates(
                     SupportedRatesTLV::try_from_ctx(from, ()).map(|(tlv, _)| tlv)?,
@@ -59,19 +62,19 @@ impl TryIntoCtx for IEEE80211TLV<'_> {
     type Error = scroll::Error;
     fn try_into_ctx(self, buf: &mut [u8], _ctx: ()) -> Result<usize, Self::Error> {
         match self {
-            Self::SSID(tlv) => buf.pwrite(
-                RawIEEE80211TLV {
+            Self::SSID(payload) => buf.pwrite(
+                TypedIEEE80211TLV {
                     tlv_type: TLVType::SSID,
-                    data: tlv,
-                    ..Default::default()
+                    payload,
+                    _phantom: PhantomData
                 },
                 0,
             ),
-            Self::SupportedRates(tlv) => buf.pwrite(
-                RawIEEE80211TLV {
-                    tlv_type: TLVType::SSID,
-                    data: tlv,
-                    ..Default::default()
+            Self::SupportedRates(payload) => buf.pwrite(
+                TypedIEEE80211TLV {
+                    tlv_type: TLVType::SupportedRates,
+                    payload,
+                    _phantom: PhantomData
                 },
                 0,
             ),
