2b1152909ed077347389b78954e1c6eacedb02fa
Frostie314159 <iam.an.programmer@gmail.com>
2024-06-06 11:10:56 +0200

    Added basic testing.

---
+++ tests/elements/mod.rs
@@ -0,0 +1,23 @@
+mod ssid;
+
+#[macro_export]
+macro_rules! gen_element_rw_test {
+    ($test_name:ident, $element:ty, $expected_element:expr, $expected_bytes:expr) => {
+        #[test]
+        fn $test_name() {
+            use ::scroll::{Pread, Pwrite, ctx::MeasureWith};
+
+            let read_element = $expected_bytes.pread::<$element>(0).unwrap();
+            assert_eq!(read_element, $expected_element, "The {} read from the supplied bytes didn't match, what was expected. 
+                Either check the test case for correctness or the TryFromCtx implementation.", stringify!($element));
+
+            let expected_length = read_element.measure_with(&());
+            let mut buf = ::std::vec![0x00; expected_length];
+            let written = buf.pwrite($expected_element, 0).unwrap();
+            assert_eq!(written, expected_length, "The amount of bytes, written by TryIntoCtx, for {}, didn't match the amount returned by MeasureWith. 
+                Either check the test case for correctness or the TryIntoCtx and or MeasureWith implementation.", stringify!($element));
+            assert_eq!(buf, $expected_bytes, "The bytes, written by TryIntoCtx, for {}, didn't match what was expected.
+                Either check the test case for correctness or the TryIntoCtx implementation.", stringify!($element));
+        }
+    };
+}
---
+++ tests/elements/ssid.rs
@@ -0,0 +1,45 @@
+use ieee80211::{elements::SSIDElement, ssid};
+
+use crate::gen_element_rw_test;
+
+// We can't test the [ssid] macro, since rust doesn't support expected build failures.
+// This isn't doesn't really matter, since it's short enough to audit by hand.
+
+pub const EXPECTED_SSID_STRING: &str = "OpenRF";
+pub const EXPECTED_SSID_ELEMENT: SSIDElement = ssid!(EXPECTED_SSID_STRING);
+pub const EXPECTED_SSID_ELEMENT_BYTES: &[u8] = EXPECTED_SSID_STRING.as_bytes();
+pub const WILDCARD_SSID_ELEMENT: SSIDElement = ssid!("");
+// One byte too long.
+pub const INVALID_SSID_STRING: &str = "Lorem ipsum dolor sit amet augue.";
+
+gen_element_rw_test!(
+    test_ssid_element_rw,
+    SSIDElement,
+    EXPECTED_SSID_ELEMENT,
+    EXPECTED_SSID_ELEMENT_BYTES
+);
+
+#[test]
+fn test_ssid_element_misc() {
+    assert!(
+        WILDCARD_SSID_ELEMENT.is_hidden(),
+        "Wildcard SSID wasn't marked as hidden. How did this happen?"
+    );
+    // Not so fun fact: This test technically already caught an error, since I screwed up when writing the original function...
+    assert_eq!(
+        EXPECTED_SSID_STRING.as_bytes().len(),
+        EXPECTED_SSID_ELEMENT.length_in_bytes(),
+        "Length in bytes returned didn't match what was expected."
+    );
+    assert_eq!(
+        SSIDElement::new(EXPECTED_SSID_STRING),
+        Some(
+            SSIDElement::new_unchecked(EXPECTED_SSID_STRING)
+        ),
+        "Creating a SSID element, with a valid SSID failed."
+    );
+    assert!(
+        SSIDElement::new(INVALID_SSID_STRING).is_none(),
+        "Creating a SSID element, with an invalid SSID succeeded."
+    );
+}
