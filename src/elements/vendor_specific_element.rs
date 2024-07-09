f452c2faf8ce90e21280742168bb69a48447a270
Frostie314159 <frostie.neuenhausen@gmail.com>
2024-07-09 17:33:04 +0200

    Made vendor specific element generic.

--- src/elements/vendor_specific_element.rs
+++ src/elements/vendor_specific_element.rs
@@ -1,3 +1,5 @@
+use core::marker::PhantomData;
+
 use scroll::{
     ctx::{MeasureWith, TryFromCtx, TryIntoCtx},
     Pread, Pwrite,
@@ -7,15 +9,18 @@ use super::{Element, ElementID};
 
 #[derive(Clone, Copy, Debug, Default, PartialEq, Eq, Hash)]
 /// The vendor specific element carries information, which is not defined in IEEE 802.11, but rather by individual vendors.
-pub struct VendorSpecificElement<'a> {
+pub struct VendorSpecificElement<'a, Payload = &'a [u8]> {
     /// The OUI of the vendor.
     pub oui: [u8; 3],
+    /// The subtype of the element.
+    pub subtype: u8,
     /// The payload of the frame.
-    pub payload: &'a [u8],
+    pub payload: Payload,
+    pub _phantom: PhantomData<&'a ()>,
 }
-impl MeasureWith<()> for VendorSpecificElement<'_> {
-    fn measure_with(&self, _ctx: &()) -> usize {
-        3 + self.payload.len()
+impl<Payload: MeasureWith<()>> MeasureWith<()> for VendorSpecificElement<'_, Payload> {
+    fn measure_with(&self, ctx: &()) -> usize {
+        4 + self.payload.measure_with(ctx)
     }
 }
 impl<'a> TryFromCtx<'a> for VendorSpecificElement<'a> {
@@ -24,18 +29,28 @@ impl<'a> TryFromCtx<'a> for VendorSpecificElement<'a> {
         let mut offset = 0;
 
         let oui = from.gread(&mut offset)?;
+        let subtype = from.gread(&mut offset)?;
         let payload_len = from.len() - offset;
         let payload = from.gread_with(&mut offset, payload_len)?;
 
-        Ok((Self { oui, payload }, offset))
+        Ok((
+            Self {
+                oui,
+                subtype,
+                payload,
+                _phantom: PhantomData,
+            },
+            offset,
+        ))
     }
 }
-impl TryIntoCtx for VendorSpecificElement<'_> {
+impl<Payload: TryIntoCtx<Error = scroll::Error>> TryIntoCtx for VendorSpecificElement<'_, Payload> {
     type Error = scroll::Error;
     fn try_into_ctx(self, buf: &mut [u8], _ctx: ()) -> Result<usize, Self::Error> {
         let mut offset = 0;
 
         buf.gwrite(self.oui, &mut offset)?;
+        buf.gwrite(self.subtype, &mut offset)?;
         buf.gwrite(self.payload, &mut offset)?;
 
         Ok(offset)
