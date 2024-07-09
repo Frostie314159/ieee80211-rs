b4a065864271a547ee012566eb22efdd7ba8d5a6
Frostie314159 <frostie.neuenhausen@gmail.com>
2024-07-09 17:31:12 +0200

    Removed frame tests.

--- tests/frames/beacon.rs
+++
@@ -1,8 +0,0 @@
-use ieee80211::{
-    mgmt_frame::{header::ManagementFrameHeader, ManagementFrame},
-    IEEE80211Frame,
-};
-
-const EXPECTED_BEACON_FRAME: IEEE80211Frame = ManagementFrame {
-    header: ManagementFrameHeader {},
-};
